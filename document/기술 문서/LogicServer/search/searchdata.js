a6192db9c39d6bcebc2d745302d26e7297940fb4
DESKTOP-803NM8V\user <69277660@naver.com>
2017-02-23 14:09:08 +0900

    기술문서 업데이트 : 로직, 채팅

---
+++ document/기술 문서/LogicServer/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "_bcdilmnpqrstv",
+  1: "_bcdilmnpqrstv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
